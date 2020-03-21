37d719b8ccfa1048706bf776710d6d9a13970495
Pepe Melendez <pepe_ciro@hotmail.com>
2020-03-21 15:56:19 -0700

    Updates

---
+++ precache-manifest.6805e9aefe52da0d188141802199d370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a625f8db3bc09980327e15f4c718ac1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "964bee52aaed111340da",
+    "url": "/react-gh-pages/static/js/2.ab901e3f.chunk.js"
+  },
+  {
+    "revision": "2bb4dbe450cc587d04b0c2d13b63c672",
+    "url": "/react-gh-pages/static/js/2.ab901e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb682a0342ac5a30009a",
+    "url": "/react-gh-pages/static/js/main.82d8c432.chunk.js"
+  },
+  {
+    "revision": "003c5f11052a1bbf47d8",
+    "url": "/react-gh-pages/static/js/runtime-main.9e95550f.js"
+  }
+]);
